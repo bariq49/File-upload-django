c183d9945b06156c8c07fbf32848742e0a7c8de3
Mudassar Amin <mudasiramin456@gmail.com>
2023-08-12 07:59:02 +0500

    commit

---
+++ project/main/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import *
+import shutil
+
+class FilesList_Serializer(serializers.Serializer):
+    files = serializers.ListField(
+        child = serializers.FileField(max_length = 10000,  allow_empty_file = False, use_url = False)
+    )
+    folder = serializers.CharField(required = False)
+
+    def zip_files(self, folder):
+        shutil.make_archive(f'static/zip/{folder}', "zip", f"media/{folder}")
+
+    def create(self, validated_data):
+        folder = Folder.objects.create()
+        files_objs = []
+        files = validated_data.pop('files')
+        for file in files:
+            file_obj = Files.objects.create(folder = folder, file = file)
+            files_objs.append(file_obj)
+        
+        self.zip_files(folder.uid)
+
+        return {'files' : {}, 'folder': str(folder.uid)}
---
+++ project/main/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import *
+from .serializers import *
+
+
+class HandleFilesUpload(APIView):
+  
+    def post(self, request):
+        try:
+            data = request.data
+            serializer = FilesList_Serializer(data = data)
+            
+            if serializer.is_valid():
+                serializer.save()
+                return Response({
+                    'status': True,
+                    'message': "Files uploaded successfully",
+                    'data': serializer.data
+                })
+            
+            return Response({
+                    'status': False,
+                    'message': serializer.errors,
+                    'data':serializer.data
+                })
+        except Exception as e:
+            print(e)
+        return Response({
+                    'status': False,
+                    'message': "Something went wrong!",
+                    'data':{}
+                })
